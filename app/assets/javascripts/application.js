28e66ec2d79f3727324690df6d6b70e365fd84f3
Alexa Lomeu <alomeufernandes@gmail.com>
2025-11-11 15:35:22 -0300

    Ajusta botão para aprovar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,38 +4,30 @@
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
 // vendor/assets/javascripts directory can be referenced here using a relative path.
 //
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
 
-import "bootstrap"
-import "./sidebar"
+// Se o bootstrap for via Sprockets (não via importmap)
+//= require bootstrap
+//= require sidebar
 
-import { createIcons, LayoutDashboard, Settings, Building2, UserCheck, MessageSquare, CreditCard, LogOut, Building, Menu, X } from "lucide";
-
-document.addEventListener("turbo:load", () => {
-  createIcons({
-    icons: {
-      LayoutDashboard,
-      Settings,
-      Building2,
-      UserCheck,
-      MessageSquare,
-      CreditCard,
-      LogOut,
-      Building,
-      Menu,
-      X,
-    },
-  });
+document.addEventListener("turbolinks:load", () => {
+  if (window.createIcons) {
+    window.createIcons({
+      icons: {
+        LayoutDashboard,
+        Settings,
+        Building2,
+        UserCheck,
+        MessageSquare,
+        CreditCard,
+        LogOut,
+        Building,
+        Menu,
+        X,
+      },
+    })
+  }
 });
-
-
-
